d29a096401364462c5b2d86620f446002e65cd36
AdvisoryFlown <120481326+GamingCentral@users.noreply.github.com>
2023-12-25 16:10:42 +0530

    dropdown works fine

--- mainAppFrontEnd.py
+++ mainAppFrontEnd.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import mainAppBackend as backend
+import json
 '''from loginFrame import Login
 from menuFrame import Men'''
 #import connectionpool as cp
@@ -37,6 +38,7 @@ class mainApp:
         self.loginFrame = ttk.Frame(root,style='TFrame')
         self.menuFrame = ttk.Frame(root,style='TFrame')
         self.pool = None
+        self.faculty_selected = None
         #self.admin = 0 #default
         self.dbFrameInitalizer()
         self.loginFrameInitializer()
@@ -322,7 +324,17 @@ class mainApp:
         self.loginFrameSwitch()
 
     def dropdown(self):
-        print('dropdown here')
+        self.dropdownFrame = tk.Toplevel(self.visitorEntry)
+        self.dropdownFrame.title("Faculty Dropdown")
+        self.dropdownFrame.configure(bg='#ebedf0')
+        self.dropdownFrame.resizable(False,False)
+        self.faculty_data = read_faculty_data_from_json("json_data.json")
+        def callback(selected_value):
+            self.dropdownFrame.destroy()
+            self.faculty_selected = selected_value
+            print(self.faculty_selected)
+        
+        faculty_data_entry = FacultyDataEntryFrame(self.dropdownFrame, callback, self.faculty_data)
 
     def submit_press_visitorEntry(self):
         print('visitor entry data')
@@ -363,8 +375,82 @@ class mainApp:
         except Exception as e:
             print(e)
 
-    def open_faculty_frame(self):
-        print('drop down')
+class FacultyDataEntryFrame(tk.Frame):
+    def __init__(self, master=None, callback=None, faculty_data=None):
+        super().__init__(master)
+        self.master = master
+        self.callback = callback
+        self.faculty_data = faculty_data or []
+        self.pack()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Label for branch combo box
+        self.branch_label = ttk.Label(self, text="Branch:")
+        self.branch_label.grid(row=0, column=0, padx=10, pady=10)
+
+        # Create StringVars for the filters
+        self.branch_var = tk.StringVar()
+        self.phno_var = tk.StringVar()
+
+        # Combo box for selecting the branch
+        self.branch_combo_box = ttk.Combobox(self, state="readonly", textvariable=self.branch_var)
+        self.branch_combo_box.grid(row=0, column=1, padx=10, pady=10)
+        branches = set(faculty["dept"] for faculty in self.faculty_data)
+        self.branch_combo_box['values'] = [""] + list(branches)  # Add an empty option for no selection
+        self.branch_var.trace_add("write", self.update_name_combo_box)  # Attach a callback to update name combo box
+
+        # Label for phone number entry
+        self.phno_label = ttk.Label(self, text="Phone Number:")
+        self.phno_label.grid(row=0, column=2, padx=10, pady=10)
+
+        # Entry field for entering phone number
+        self.phno_entry = ttk.Entry(self, textvariable=self.phno_var)
+        self.phno_entry.grid(row=0, column=3, padx=10, pady=10)
+        self.phno_var.trace_add("write", self.update_name_combo_box)  # Attach a callback to update name combo box
+
+        # Combo box for selecting the name
+        self.name_label = ttk.Label(self, text="Name:")
+        self.name_label.grid(row=1, column=0, padx=10, pady=10)
+
+        self.name_combo_box = ttk.Combobox(self, state="readonly")
+        self.name_combo_box.grid(row=1, column=1, padx=10, pady=10, columnspan=2)
+        self.name_combo_box.bind("<<ComboboxSelected>>", self.on_name_select)  # Attach a callback for selection
+
+        # Show all names by default
+        all_names = [faculty["name"] for faculty in self.faculty_data]
+        self.name_combo_box['values'] = all_names
+
+    def update_name_combo_box(self, *args):
+        # Update the name combo box values based on selected branch and phno
+        selected_branch = self.branch_var.get()
+        selected_phno = self.phno_var.get()
+
+        if not selected_branch and not selected_phno:
+            # If no branch or phno is selected, show all names
+            all_names = [faculty["name"] for faculty in self.faculty_data]
+            self.name_combo_box['values'] = all_names
+        else:
+            # Filter names based on selected branch and phno
+            filtered_names = [faculty["name"] for faculty in self.faculty_data
+                              if (not selected_branch or faculty["dept"] == selected_branch) and
+                              (not selected_phno or faculty["phno"] == selected_phno)]
+
+            self.name_combo_box['values'] = filtered_names
+
+    def on_name_select(self, event):
+        # Store the selected value in a variable or perform any desired action
+        selected_value = self.name_combo_box.get()
+        print(f"Selected Name: {selected_value}")
+        # Pass the selected value to the callback function
+        if self.callback:
+            self.callback(selected_value)
+
+def read_faculty_data_from_json(file_path):
+    with open(file_path, 'r') as file:
+        faculty_data = json.load(file)
+    return faculty_data
 
 if __name__=='__main__':
     root = tk.Tk()
